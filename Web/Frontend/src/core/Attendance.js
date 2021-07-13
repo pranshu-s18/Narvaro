d5da9f09676560d5827325d3b964d3baa2ee0e5a
Pranshu Saxena <pranshutejas@gmail.com>
2021-07-13 17:14:14 +0530

    Display data in table

--- Web/Frontend/src/core/Attendance.js
+++ Web/Frontend/src/core/Attendance.js
@@ -1,63 +1,70 @@
-import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { attendanceAPI, isAuthenticated } from "../auth/helper";
 import Base from "./Base";
 
-const Section = (data) => (
-  <table className="table table-responsive text-white text-center">
-    <thead>
-      <tr>
-        <th>Roll No.</th>
-        <th>Attendance</th>
-        <th>Time</th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((record, id) => (
-        <tr key={id} className="spaced-table">
-          <td>{record.rollNo}</td>
-          <td>{record.present ? "P" : "A"}</td>
-          <td>{moment(record.date)}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+import moment from "moment";
+import "moment/locale/en-in";
+moment.updateLocale("en-in", { week: { dow: 1 } });
 
-const SubSection = (data, id) => (
-  <tbody key={id}>
-    {data.map((att, id2) => (
-      <tr key={id2} className="spaced-table">
-        <td>{att.rollNo}</td>
-        <td>{att.present ? "Present" : "Absent"}</td>
-        <td>{moment(att.date).format("hh:mm A")}</td>
-      </tr>
-    ))}
-  </tbody>
-);
+const dates = () => {
+  const start = moment().startOf("week");
+  const list = [start.toDate()];
+  for (let i = 0; i < 6; i++) list.push(start.add(1, "day").toDate());
+  return list;
+};
 
 const Attendance = () => {
   const { user, token } = isAuthenticated();
-  const [dataStore, setDataStore] = useState();
   const [attendance, setAttendance] = useState([]);
   const [hostel, setHostel] = useState("BH2");
+  const [dateList, setDateList] = useState([]);
 
   useEffect(() => {
     attendanceAPI({ id: user._id, token, hostel }).then((data) => {
       if (data.error) console.log(data.error);
-      else {
-        setDataStore(data);
-        data.forEach((el) =>
-          el.attendance = el.attendance.filter((e) =>
-            moment(e.date).isBetween(moment().subtract(1, "day"), moment())
-          )
-        );
-        setAttendance(data);
-      }
+      else setAttendance(data);
     });
+
+    setDateList(dates());
   }, []);
 
-  return <Base className="py-4"></Base>;
+  const attRender = (data) => {
+    let ar = [false, false, false, false, false, false, false];
+    data.forEach((el) => (ar[moment(el.date).day() - 1] = moment(el.date)));
+
+    return ar.map((el, i) => {
+      if (i >= moment().day()) return <td key={i}>-</td>;
+      else {
+        if (el) return <td key={i}>{el.format("hh:mm A")}</td>;
+        else return <td key={i}>A</td>;
+      }
+    });
+  };
+
+  return (
+    <Base className="py-4">
+      <div className="container">
+        <table className="table table-responsive text-white text-center">
+          <thead>
+            <tr>
+              <th>Roll No</th>
+              {dateList.map((date, id) => (
+                <th key={id}>{moment(date).format("DD MMM")}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {attendance.map((stu, id1) => (
+              <tr key={id1}>
+                <td>{stu.rollNo}</td>
+                {attRender(stu.attendance)}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Base>
+  );
 };
 
 export default Attendance;
