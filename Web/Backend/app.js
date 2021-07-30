7891f72107129d2403d7060b4bca679a20f6d329
Pranshu Saxena <pranshutejas@gmail.com>
2021-07-30 21:23:16 +0530

    Changes for Deployment - Removed CORS, dotenv and Added frontend build

--- Web/Backend/app.js
+++ Web/Backend/app.js
@@ -1,15 +1,12 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
-
 const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -19,13 +16,11 @@ mongoose
   .catch((e) => console.log("Database Connection Failed", e));
 
 const app = express();
+app.use(express.static("client"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", methods: ["POST"] }));
-
 app.use("/api", authRouter);
 app.use("/api", adminRouter);
 app.use("/app", userRouter);
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server started at ${PORT}`));
+app.listen(8000, () => console.log("Server started at 8000"));
