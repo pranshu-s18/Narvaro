25f81dfbb8215552790a64bb6a9629c9ee8396ae
Pranshu Saxena <pranshutejas@gmail.com>
2022-02-02 12:42:36 +0530

    Squashed 'Web/Frontend/' content from commit 717afe6
    git-subtree-dir: Web/Frontend
    git-subtree-split: 717afe6ad925da639d6928eea9aeabc9a49346ec

---
+++ src/auth/helper.js
@@ -0,0 +1,55 @@
+import { API } from "../core/Commons";
+
+export const loginAPI = (user) =>
+  fetch(`${API}/login`, {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  })
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+
+export const auth = (data, next) => {
+  if (typeof window !== "undefined")
+    localStorage.setItem("jwt", JSON.stringify(data));
+  next();
+};
+
+export const registerAPI = (user) =>
+  fetch(`${API}/register`, {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  })
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+
+export const attendanceAPI = (id, token, data) =>
+  fetch(`${API}/attendance/${id}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: data,
+  })
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") return false;
+  else
+    return localStorage.getItem("jwt")
+      ? JSON.parse(localStorage.getItem("jwt"))
+      : false;
+};
+
+export const logout = () => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    return fetch(`${API}/logout`, { method: "GET" }).catch((e) =>
+      console.log(e)
+    );
+  }
+};
